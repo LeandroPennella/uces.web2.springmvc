2433607143bd6d0df2878dda51cfb9cf1c0d82c1
Leandro <leandropennella@yahoo.com.ar>
2015-09-02 21:11:30 -0300

    modificacion

---
+++ springmvc/src/ar/edu/uces/progweb2/springmvc/controller/simple/PersonaController.java
@@ -0,0 +1,27 @@
+package ar.edu.uces.progweb2.springmvc.controller.simple;
+
+import java.util.*;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.uces.progweb2.springmvc.model.*;
+
+@Controller
+public class PersonaController {
+
+	@RequestMapping("/listarPersonas")
+	public ModelAndView crearLista()
+	{
+		List<Persona>personas=new ArrayList<Persona>();
+		personas.add(new Persona("pepe"));
+		personas.add(new Persona("jose"));
+		personas.add(new Persona("lean"));
+		personas.add(new Persona("jorge"));
+		ModelAndView mv=new ModelAndView("/views/listadoDePersonas.jsp");
+		mv.addObject("personas", personas);
+		return mv;
+	}
+}
---
+++ springmvc/src/ar/edu/uces/progweb2/springmvc/model/Persona.java
@@ -0,0 +1,10 @@
+package ar.edu.uces.progweb2.springmvc.model;
+
+public class Persona {
+private String nombre;
+public Persona(String nombre){this.nombre=nombre;}
+
+
+public String getNombre(){return this.nombre;}
+
+}
