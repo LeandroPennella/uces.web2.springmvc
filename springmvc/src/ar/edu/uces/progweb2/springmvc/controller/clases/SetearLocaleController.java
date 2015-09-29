a1c41c80d4f52f351ff16634fee66eff9829c37d
leandrop <leandropennella@yahoo.com.ar>
2015-09-29 17:51:53 -0300

    internacionalizacion bonillo

--- springmvc/src/ar/edu/uces/progweb2/springmvc/controller/clases/SetearLocaleController.java
+++ springmvc/src/ar/edu/uces/progweb2/springmvc/controller/clases/SetearLocaleController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.jstl.core.Config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,6 +19,7 @@ public class SetearLocaleController {
 	@RequestMapping(value = "/SetearLocaleController")
 	public String testeo(HttpServletRequest rs, HttpServletRequest request, HttpServletResponse response) {
 		localeResolver.setLocale(request, response, new Locale("es_AR"));
+		//Config.set(request.getSession(), Config.FMT_LOCALE, new java.util.Locale("es"));
 		System.out.println("MyController - hola mundo");
 		System.out.println("mensaje: " + rs.getAttribute("mensaje"));
 		return "/views/internacionalizacion/holamundo.jsp";
