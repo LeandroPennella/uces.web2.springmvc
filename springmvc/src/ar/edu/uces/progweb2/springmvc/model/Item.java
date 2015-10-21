f3131f128e7def3db4ba6f0402c373c69d1473b1
Alumno <Alumno@CE100_02>
2015-10-21 20:07:12 -0300

    Item

---
+++ springmvc/src/ar/edu/uces/progweb2/springmvc/model/Item.java
@@ -0,0 +1,44 @@
+package ar.edu.uces.progweb2.springmvc.model;
+
+public class Item {
+
+	private int id;
+	private String descripcion;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((descripcion == null) ? 0 : descripcion.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (descripcion == null) {
+			if (other.descripcion != null)
+				return false;
+		} else if (!descripcion.equals(other.descripcion))
+			return false;
+		return true;
+	}
+	
+}
